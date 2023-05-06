514adc258f84cf481fc0387ce9fbe7c5479a24b9
Zii9 <132731744+Zii9@users.noreply.github.com>
2023-05-06 12:07:56 +0530

    Update Bulid.gradle.kts

--- Bulid.gradle.kts
+++ Bulid.gradle.kts
@@ -37,7 +37,7 @@ import com.lagradost.cloudstream3.gradle.CloudstreamExtension
      cloudstream { 
          // when running through github workflow, GITHUB_REPOSITORY should contain current repository name 
          // you can modify it to use other git hosting services, like gitlab 
-         setRepo(System.getenv("GITHUB_REPOSITORY") ?: "https://github.com/duongnv1996/bongngo-cs3-extension") 
+         setRepo(System.getenv("GITHUB_REPOSITORY") ?: "https://github.com/zii9/Xmoviesforyou") 
      } 
   
      android { 
